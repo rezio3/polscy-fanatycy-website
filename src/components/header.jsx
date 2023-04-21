2862482b914a1413a59852a9faf31a725b74dbc6
Joszko <jakub.rezler96@gmail.com>
2023-04-22 01:07:48 +0200

    update header component

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -7,6 +7,17 @@ const Header = () => {
 			<div className="shadow">
 				<div className="header-content">
 					<h1>Polscy Fanatycy</h1>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime
+						inventore repudiandae odio quod nesciunt cum voluptates. Eveniet,
+						aliquam. Accusantium, accusamus autem omnis numquam iure dolorum
+						tempore hic, totam eum quos distinctio mollitia adipisci esse
+						perferendis, perspiciatis nobis! Quo quam rem sed eveniet.
+						Consequuntur officia error accusantium amet modi cum aliquam?
+					</p>
+					<a href="https://www.google.pl">
+						Read more <span class="material-symbols-outlined arrow">east</span>
+					</a>
 				</div>
 			</div>
 		</div>
