4f2d21d73e4d86987a287204e9a0671e2801a1f5
Joszko <jakub.rezler96@gmail.com>
2023-04-22 00:26:33 +0200

    update header component

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import "../style/css/header.css";
 
 const Header = () => {
-	return <div className="header-container"></div>;
+	return (
+		<div className="header-container">
+			<div className="shadow">
+				<div className="header-content">
+					<h1>Polscy Fanatycy</h1>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Header;
