b7ca089059de4500624f654d9c17a258b0bcc967
Joszko <jakub.rezler96@gmail.com>
2023-04-22 00:26:23 +0200

    add font

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navigation from "./components/navigation";
+import Header from "./components/header";
 
 function App() {
 	return (
 		<div className="App">
 			<Navigation />
+			<Header />
 		</div>
 	);
 }
