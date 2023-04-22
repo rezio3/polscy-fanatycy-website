bc1ce5cad50f4bd5de7cfa078f0dd22c840bc31c
Joszko <jakub.rezler96@gmail.com>
2023-04-22 23:34:44 +0200

    update mostReadSection component

--- src/components/mostReadSection.jsx
+++ src/components/mostReadSection.jsx
@@ -2,7 +2,82 @@ import React from "react";
 import "../style/css/mostReadSection.css";
 
 const MostReadSection = () => {
-	return <div className="most-read-section-container"></div>;
+	return (
+		<div className="most-read-section-container">
+			<h2 className="most-read-section-header">Most read this week</h2>
+			<div className="boxes-container">
+				<div className="read-box">
+					<h3>When you can tell your customer the truth about technology?</h3>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+						Architecto, beatae consequatur ducimus quis, excepturi amet ab rerum
+						consequuntur!
+					</p>
+					<a href="https://www.google.pl">
+						Read more <span class="material-symbols-outlined arrow">east</span>
+					</a>
+				</div>
+				<div className="read-box">
+					<h3>Why these technologies will not leave you indifferent?</h3>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident
+						similique modi nobis quia et aperiam soluta ut voluptatem, dolores,
+						voluptates.
+					</p>
+					<a href="https://www.google.pl">
+						Read more <span class="material-symbols-outlined arrow">east</span>
+					</a>
+				</div>
+				<div className="read-box">
+					<h3>
+						This will help you become the first and strengthen your position
+					</h3>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident
+						similique modi nobis quia et aperiam soluta ut voluptatem, dolores,
+						voluptates.
+					</p>
+					<a href="https://www.google.pl">
+						Read more <span class="material-symbols-outlined arrow">east</span>
+					</a>
+				</div>
+				<div className="read-box">
+					<h3>
+						This will help you become the first and strengthen your position
+					</h3>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident
+						similique modi nobis quia et aperiam soluta ut voluptatem, dolores,
+						voluptates.
+					</p>
+					<a href="https://www.google.pl">
+						Read more <span class="material-symbols-outlined arrow">east</span>
+					</a>
+				</div>
+				<div className="read-box">
+					<h3>You just have to try this novelty</h3>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident
+						similique modi nobis quia et aperiam soluta ut voluptatem, dolores.
+					</p>
+					<a href="https://www.google.pl">
+						Read more <span class="material-symbols-outlined arrow">east</span>
+					</a>
+				</div>
+				<div className="read-box">
+					<h3>What are the benefits of this product or service?</h3>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident
+						similique modi nobis quia et aperiam soluta ut voluptatem, dolores,
+						voluptates.
+					</p>
+					<a href="https://www.google.pl">
+						Read more <span class="material-symbols-outlined arrow">east</span>
+					</a>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default MostReadSection;
