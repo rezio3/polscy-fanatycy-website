431b7818c27f42f40890cd894f39af7661164f62
Joszko <jakub.rezler96@gmail.com>
2023-04-24 14:53:53 +0200

    update contact.jsx

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import "../style/css/contact.css";
+import ballImg from "../img/contact-ball.jpg";
 
 const Contact = () => {
-	return <div className="contact-container"></div>;
+	return (
+		<div className="contact-container">
+			<img src={ballImg} width="400px" />
+			<div className="contact-text-container">
+				<span>Stay informed with us</span>
+				<h2>Stay up to date with news from the world of technology</h2>
+				<span>Sign up for our Newsletter!</span>
+			</div>
+		</div>
+	);
 };
 
 export default Contact;
