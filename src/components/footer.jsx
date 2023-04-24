b371e522bd64173557f34eba4a37315b80f240c3
Joszko <jakub.rezler96@gmail.com>
2023-04-24 15:31:41 +0200

    update footer.jsx component

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,7 +2,27 @@ import React from "react";
 import "../style/css/footer.css";
 
 const Footer = () => {
-	return <footer className="footer-container"></footer>;
+	return (
+		<footer className="footer-container">
+			<span className="copyright">
+				Polscy Fanatycy 2023 &copy; All Right Reserved
+			</span>
+			<div className="icons-container">
+				<a href="https://www.instagram.com/polscyfanatycy_/">
+					<i className="fa-brands fa-instagram"></i>
+				</a>
+				<a href="https://www.facebook.com/PFpolscyfanatycy">
+					<i class="fa-brands fa-square-facebook"></i>
+				</a>
+				<a href="https://www.youtube.com/channel/UCXBEVpYn4wxI3kMbGq3KPtg">
+					<i class="fa-brands fa-youtube"></i>
+				</a>
+				<a href="https://www.google.com">
+					<i class="fa-brands fa-tiktok"></i>
+				</a>
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
