0d2313fbfa40d364d9fa3596ce28214dade74ff7
Joszko <jakub.rezler96@gmail.com>
2023-04-24 15:16:30 +0200

    update contact.jsx component

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -5,11 +5,12 @@ import ballImg from "../img/contact-ball.jpg";
 const Contact = () => {
 	return (
 		<div className="contact-container">
-			<img src={ballImg} width="400px" />
+			<img src={ballImg} width="400px" className="contact-img" />
 			<div className="contact-text-container">
 				<span>Stay informed with us</span>
-				<h2>Stay up to date with news from the world of technology</h2>
-				<span>Sign up for our Newsletter!</span>
+				<h2>Stay up to date, collaborate and help us grow!</h2>
+				<span>Just send up your will</span>
+				<button className="contact-btn">Write us!</button>
 			</div>
 		</div>
 	);
