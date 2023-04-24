072f544bc8ef258185a66770412981a4139299a0
Joszko <jakub.rezler96@gmail.com>
2023-04-24 02:13:11 +0200

    add instagram icon

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "../style/css/navigation.css";
 
 const Navigation = () => {
-	return <div className="nav-bar"></div>;
+	return (
+		<div className="nav-bar">
+			<i className="fa-brands fa-instagram"></i>
+		</div>
+	);
 };
 
 export default Navigation;
